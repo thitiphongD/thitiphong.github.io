89e81d511b3008b8799a856288790fb78267676e
thitiphongD <80618380+thitiphongD@users.noreply.github.com>
2022-02-18 13:53:28 +0700

    add homework

--- intro-to-js/daew.js
+++ intro-to-js/daew.js
@@ -11,3 +11,15 @@ function helloWorld() {
     const h1message = document.getElementById("demo").innerHTML
     alert(h1message + " We are family")
 }
+
+function calAge() {
+    var inputAge = prompt("เกิดปีหนายยยยย")
+    alert('ยินดีด้วย นายอยู่ในประเทศนี้มา ' + (2022 - inputAge) + ' ปีแล้ว')
+}
+
+function loadProfile() {
+    var name = prompt("name?")
+    var province = prompt("province?")
+    document.getElementById("my-name").innerHTML = name
+    document.getElementById("my-province").innerHTML = province
+}
